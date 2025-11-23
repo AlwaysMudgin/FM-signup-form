7f9199acd08d2a79bb54c38a3c13ed8b7fcad2c7
AlwaysMudgin <193750882+AlwaysMudgin@users.noreply.github.com>
2025-11-23 18:26:42 -0500

    Initial solution

--- src/components/SignupCard/SignupCard.jsx
+++ src/components/SignupCard/SignupCard.jsx
@@ -18,45 +18,39 @@ function SignupCard() {
     setSuccess(false);
   }
 
+  if (success) {
+    return <SuccessDialog email={acceptedEmail} dismiss={handleDismiss} />;
+  }
+
   return (
     <article className="card">
-      {success && (
-        <SuccessDialog email={acceptedEmail} dismiss={handleDismiss} />
-      )}
-      {!success && (
-        <>
-          <div className="card__illustration-container">
-            <picture>
-              <source
-                srcSet={illustrationTablet}
-                media="(min-width: 648px) and (max-width: 1023px)"
-              />
-              <source
-                srcSet={illustrationDesktop}
-                media="(min-width: 1024px)"
-              />
-              <img
-                src={illustrationMobile}
-                alt="illustration of screens and data"
-              />
-            </picture>
-          </div>
-          <section className="card__content-container">
-            <div className="card__text-content">
-              <h1 className="card__heading">Stay updated!</h1>
-              <p className="card__body">
-                Join 60,000+ product managers receiving monthly updates on:
-              </p>
-              <ul className="card__ul">
-                <li>Product discovery and building what matters</li>
-                <li>Measuring to ensure updates are a success</li>
-                <li>And much more!</li>
-              </ul>
-            </div>
-            <EmailForm handleSuccess={handleSuccess} />
-          </section>
-        </>
-      )}
+      <div className="card__illustration-container">
+        <picture>
+          <source
+            srcSet={illustrationTablet}
+            media="(min-width: 648px) and (max-width: 1023px)"
+          />
+          <source srcSet={illustrationDesktop} media="(min-width: 1024px)" />
+          <img
+            src={illustrationMobile}
+            alt="illustration of screens and data"
+          />
+        </picture>
+      </div>
+      <section className="card__content-container">
+        <div className="card__text-content">
+          <h1 className="card__heading">Stay updated!</h1>
+          <p className="card__body">
+            Join 60,000+ product managers receiving monthly updates on:
+          </p>
+          <ul className="card__ul">
+            <li>Product discovery and building what matters</li>
+            <li>Measuring to ensure updates are a success</li>
+            <li>And much more!</li>
+          </ul>
+        </div>
+        <EmailForm handleSuccess={handleSuccess} />
+      </section>
     </article>
   );
 }
